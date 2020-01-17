37236d99e18f37d65b386cda2327798c49b68edc
Jacob Bremer <bremerjp@gmail.com>
2020-01-17 09:14:21 -0500

    added scroll effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,83 +5,85 @@
 */
 
 (function($) {
+  var $window = $(window),
+    $body = $("body"),
+    $header = $("#header"),
+    $banner = $("#banner");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$banner = $('#banner');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["841px", "980px"],
+    small: ["737px", "840px"],
+    xsmall: ["481px", "736px"],
+    xxsmall: [null, "480px"]
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '841px',   '980px'  ],
-			small:    [ '737px',   '840px'  ],
-			xsmall:   [ '481px',   '736px'  ],
-			xxsmall:  [ null,      '480px'  ]
-		});
+  // Play initial animations on page load.
+  $window.on("load", function() {
+    window.setTimeout(function() {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Dropdowns.
+  $("#nav > ul").dropotron({
+    alignment: "center"
+  });
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			alignment: 'center'
-		});
+  // Nav Panel.
 
-	// Nav Panel.
+  // Button.
+  $(
+    '<div id="navButton">' +
+      '<a href="#navPanel" class="toggle"></a>' +
+      "</div>"
+  ).appendTo($body);
 
-		// Button.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
+  // Panel.
+  $('<div id="navPanel">' + "<nav>" + $("#nav").navList() + "</nav>" + "</div>")
+    .appendTo($body)
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: "left",
+      target: $body,
+      visibleClass: "navPanel-visible"
+    });
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+  $(document).ready(function() {
+    $("a.scrollLink").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        { scrollTop: $($(this).attr("href")).offset().top },
+        500
+      );
+    });
+  });
 
-	// Header.
-	// If the header is using "alt" styling and #banner is present, use scrollwatch
-	// to revert it back to normal styling once the user scrolls past the banner.
-	// Note: This is disabled on mobile devices.
-		if (!browser.mobile
-		&&	$header.hasClass('alt')
-		&&	$banner.length > 0) {
-
-			$window.on('load', function() {
-
-				$banner.scrollex({
-					bottom:		$header.outerHeight() + 5,
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
-				});
-
-			});
-
-		}
-
+  // Header.
+  // If the header is using "alt" styling and #banner is present, use scrollwatch
+  // to revert it back to normal styling once the user scrolls past the banner.
+  // Note: This is disabled on mobile devices.
+  if (!browser.mobile && $header.hasClass("alt") && $banner.length > 0) {
+    $window.on("load", function() {
+      $banner.scrollex({
+        bottom: $header.outerHeight() + 5,
+        terminate: function() {
+          $header.removeClass("alt");
+        },
+        enter: function() {
+          $header.addClass("alt");
+        },
+        leave: function() {
+          $header.removeClass("alt");
+          $header.addClass("reveal");
+        }
+      });
+    });
+  }
 })(jQuery);
